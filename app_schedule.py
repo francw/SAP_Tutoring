287f48e490830a99cae52bbb2ec35fda0b63229d
francw <you@example.com>
2021-12-03 00:40:02 -0700

    trying heroku

--- app_schedule.py
+++ app_schedule.py
@@ -1,24 +1,13 @@
 import pandas as pd
 from datetime import timedelta
-import pyodbc
 import plotly.express as px
 import dash
 from dash import dcc
 from dash import html
 
-conn = pyodbc.connect('Driver={SQL Server};'
-                      'Server=DESKTOP-4UMM3V8\SQLEXPRESS;'
-                      'Database=SAP_test;'
-                      'Trusted_Connection=yes;')
 
+df = pd.read_csv('SAP_schedule_data.csv')
 
-sql_query = pd.read_sql_query(''' 
-                              select * from SAP_test.dbo.vwClassScheduleDetail
-                              '''
-                              ,conn) # here, the 'conn' is the variable that contains your database connection information from step 2
-
-df = pd.DataFrame(sql_query)
-# df.to_csv (r'C:\Users\franc\OneDrive\2.PythonProjects\SAP_tutoring\exported_data.csv', index = False) # place 'r' before the path name
 df["ClassDate"]= pd.to_datetime(df["ClassDate"])
 df['date2']=df['ClassDate']+timedelta(days=0.95)
 df['ClassUnique']=df['ClassGrade']+'_'+df['Subdivision']
